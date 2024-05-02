0fe146e8970d3742d147cc490c541b8e70d2bf27
cartermusee <cartermusee@gmail.com>
2024-05-02 08:42:58 +0000

    k

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv()
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
